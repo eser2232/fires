260c6dd80e2efa8427bcbb1d3fe52321bdac110c
eser_2232 <eser_2232@hotmail.com>
2024-11-14 20:43:44 +0200

    optimizing codes

--- index.js
+++ index.js
@@ -1,43 +1,53 @@
-mycan = document.getElementById("mycan");
-    ctx = mycan.getContext("2d");
-    h = window.innerHeight;
-    w = window.innerWidth;
-    mycan.height = h;
-    mycan.width = w;
+const mycan = document.getElementById("mycan");
+const ctx = mycan.getContext("2d");
+const h = window.innerHeight;
+const w = window.innerWidth;
+mycan.height = h;
+mycan.width = w;
 
+let fires = [];
+for (let a = 0; a < 100; a++) {
+    fires.push({
+        x: w / 2 - 200 * Math.random(),
+        y: h / 2,
+        r: 50 * Math.random(),
+        vy: 5 * Math.random(),
+        life: 100 * Math.random()
+    });
+}
+
+function firefunction() {
+    ctx.fillStyle = "black";
+    ctx.clearRect(0, 0, w, h);
+    ctx.fillRect(0, 0, w, h);
+    ctx.globalCompositeOperation = "lighter";
     
-    fires = [];
-    for (var a=0; a<100; a++){
-        fires.push({"x":w/2-200*Math.random(), "y":h/2, "r":50*Math.random(), "vy":5*Math.random(), "life":100*Math.random()});
-    }
-    
-    function firefunction() {
-        ctx.fillStyle="black";
-        ctx.clearRect(0,0,w,h);
-        ctx.fillRect(0,0,w,h);
-        ctx.globalCompositeOperation="lighter";
-        for (var b=0; b<100; b++){
-            var colour = ctx.createRadialGradient(fires[b].x, fires[b].y, 2, fires[b].x, fires[b].y, fires[b].r);
-                colour.addColorStop(0, "white");
-                colour.addColorStop(0.4, "yellow");
-                colour.addColorStop(0.6, "orange");
-                colour.addColorStop(1, "red");
-            ctx.fillStyle=colour;
-    
-            ctx.beginPath();
-            ctx.arc(fires[b].x, fires[b].y, fires[b].r, 0*Math.PI, 2*Math.PI);
-            ctx.fill();
-    
-            fires[b].y-=fires[b].vy;
-            fires[b].r-=0.4;
-            fires[b].life-=1;
-    
-            if (fires[b].life<0 || fires[b].r<0 || fires[b].y<100){
-                fires[b].x = w/2-200*Math.random();
-                fires[b].y = h/2;
-                fires[b].r = 50*Math.random();
-                fires[b].life = 100*Math.random();
-            }
+    fires.forEach((fire, index) => {
+        const colour = ctx.createRadialGradient(fire.x, fire.y, 2, fire.x, fire.y, fire.r);
+        colour.addColorStop(0, "white");
+        colour.addColorStop(0.4, "yellow");
+        colour.addColorStop(0.6, "orange");
+        colour.addColorStop(1, "red");
+        ctx.fillStyle = colour;
+
+        ctx.beginPath();
+        ctx.arc(fire.x, fire.y, fire.r, 0, 2 * Math.PI);
+        ctx.fill();
+
+        fire.y -= fire.vy;
+        fire.r -= 0.4;
+        fire.life -= 1;
+
+        if (fire.life < 0 || fire.r < 0 || fire.y < 100) {
+            fires[index] = {
+                x: w / 2 - 200 * Math.random(),
+                y: h / 2,
+                r: 50 * Math.random(),
+                vy: 5 * Math.random(),
+                life: 100 * Math.random()
+            };
         }
-    }
-    setInterval(firefunction,30);
+    });
+}
+
+setInterval(firefunction, 30);
